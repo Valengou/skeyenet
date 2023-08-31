47e03693c833d1bf675fb0010b8242334df2d0bb
valengou <valentingourdy@gmail.com>
2023-08-31 15:11:06 -0300

    Update loss_functions.py

--- Src/loss_functions.py
+++ Src/loss_functions.py
@@ -8,3 +8,10 @@ def dice_coef(y_true, y_pred, smooth = 1):
 
 def soft_dice_loss(y_true, y_pred):
     return 1-dice_coef(y_true, y_pred)
+
+def iou_coef(y_true, y_pred, smooth=1):
+  intersection = K.sum(K.abs(y_true * y_pred), axis=[1,2,3])
+  union = K.sum(y_true,[1,2,3])+K.sum(y_pred,[1,2,3])-intersection
+  iou = K.mean((intersection + smooth) / (union + smooth), axis=0)
+
+  return iou
